f3e21f958ab8667d5f61704764bc8c34139ca44d
ricardocotillo <ricardo.cotillo@gmail.com>
2021-11-22 16:51:35 -0500

    changes

--- www/settings.py
+++ www/settings.py
@@ -23,22 +23,11 @@ env = environ.Env(DEBUG=(bool, False))
 env_file = os.path.join(BASE_DIR, ".env")
 # environ.Env.read_env()
 
-
-if os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-    # Pull secrets from Secret Manager
-    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-
-    client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
-    name = f"projects/508686565862/secrets/{settings_name}/versions/latest"
-    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
-
-    env.read_env(io.StringIO(payload))
-elif os.path.isfile(env_file):
+if os.path.isfile(env_file):
     # Use a local secret file, if provided
     env.read_env(env_file)
 else:
-    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
+    raise Exception("No local .env detected. No secrets found.")
 # [END gaestd_py_django_secret_config]
 
 # SECURITY WARNING: keep the secret key used in production secret!
